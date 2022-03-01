0320795e3970623f67821864b3c79b65522ede74
Sajid <sajid.suthar@goniyo.com>
2022-03-01 17:12:29 +0530

    - New code added

---
+++ app/src/main/java/com/example/demoapplication/util/KotlinExtension.kt
@@ -0,0 +1,16 @@
+package com.example.demoapplication.util
+
+import android.view.View
+
+/**
+ * Created by Sajid.
+ */
+
+
+fun View.show() {
+    visibility = View.VISIBLE
+}
+
+fun View.hide() {
+    visibility = View.GONE
+}
--- app/src/main/java/com/example/demoapplication/view/activity/MainActivity.kt
+++ app/src/main/java/com/example/demoapplication/view/activity/MainActivity.kt
@@ -6,7 +6,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.demoapplication.databinding.ActivityMainBinding
 import com.example.demoapplication.model.Person
-import com.example.demoapplication.util.REGEX_EMAIL
 import com.example.demoapplication.view.adapter.TestAdapter
 import java.util.regex.Matcher
 import java.util.regex.Pattern
@@ -16,10 +15,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var mBinding : ActivityMainBinding
     private val testAdapter = TestAdapter()
     private var listOfPerson: ArrayList<Person> = ArrayList()
-    private var longString = "56hj fofg dfg56hjk 1HG CM8 5564A265324 fghdghs AA-000-AA vhgd"
+    private var longString = "56hj fog dfg56hjk 1HG CM8 5564A265324 fights AA-000-AA vhdl"
     private val patternForVin : Pattern = Pattern.compile("\\d+[A-Z]{2} [A-Z]{2}\\d+ [0-9]{4}[A-Z]\\d+")
     private val patternForPlate: Pattern = Pattern.compile("[A-Z]{2}-[0-9]{3}-[A-Z]{2}")
-    private val emailPattern = Pattern.compile(REGEX_EMAIL)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = ActivityMainBinding.inflate(layoutInflater)
@@ -29,14 +27,7 @@ class MainActivity : AppCompatActivity() {
         mBinding.rvTest.adapter = testAdapter
         updateAdapter()
 
-        mBinding.fab.setOnClickListener {
-            val newList : ArrayList<Person> = ArrayList()
-            newList.addAll(listOfPerson)
-            newList[0] = Person(1, "Intern","XYZ")
-            newList.add(Person(4, "React Native","Test"))
-            testAdapter.submitList(newList)
 
-        }
 
         setOnclickListener()
 
@@ -45,17 +36,17 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun findPattern(): String?{
-        var matcher : Matcher ? = null
+        var matcher: Matcher?
 
         matcher = patternForVin.matcher(longString)
-        if (matcher.find()) {
-            return matcher.group()
+        return if (matcher.find()) {
+            matcher.group()
         }else{
             matcher = patternForPlate.matcher(longString)
             if (matcher.find()){
-                return matcher.group()
+                matcher.group()
             }else{
-                return "NOT found"
+                "NOT found"
             }
         }
     }
@@ -71,12 +62,21 @@ class MainActivity : AppCompatActivity() {
             // Responds to chip's close icon click if one is present
         }
 
-        mBinding.chip1.setOnCheckedChangeListener { chip, isChecked ->
+        mBinding.chip1.setOnCheckedChangeListener { _, _ ->
             // Responds to chip checked/unchecked
         }
 
-        mBinding.chipGroup.setOnCheckedChangeListener { group, checkedId ->
+        mBinding.chipGroup.setOnCheckedChangeListener { _, _ ->
             // Responds to child chip checked/unchecked
+        }
+
+        mBinding.fab.setOnClickListener {
+            val newList : ArrayList<Person> = ArrayList()
+            newList.addAll(listOfPerson)
+            newList[0] = Person(1, "Intern","XYZ")
+            newList.add(Person(4, "React Native","Test"))
+            testAdapter.submitList(newList)
+
         }
     }
 
@@ -84,7 +84,9 @@ class MainActivity : AppCompatActivity() {
 
         listOfPerson.add(Person(1, "Android","Sajid"))
         listOfPerson.add(Person(2, "DotNet","Ali"))
-        listOfPerson.add(Person(3, "PHP","Suthar"))
+        listOfPerson.add(Person(3, "PHP","Star"))
+
+        listOfPerson.filter { it.id == 1 }
 
         testAdapter.submitList(listOfPerson)
 
